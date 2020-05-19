31984c73b8b09e975d845dd8990f3edddfe32f44
Yuga Nishida <yuga.nishida@nextbeat.net>
2020-05-19 17:01:09 +0900

    move main.scss

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   context: path.join(__dirname, './'),
   // ここがどのscssをみるか
-  entry: "./stylesheets/modules/main.scss",
+  entry: "./stylesheets/main.scss",
   output: {
     // どのディレクトリにどんな名前でbuild後のcssを吐くか
     path: path.join(__dirname, ''),
